2ec90d243b0a254a719fdd280407ed2ceb2587ee
lamb <yany57228@gmail.com>
2023-04-25 17:12:31 +0800

    新增api

---
+++ Myapi/Controllers/UrlController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Myapi.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UrlController : ControllerBase
+{
+    // GET
+    public IEnumerable<string> Get()
+    {
+        IList<string> urls = new List<string>();
+        urls.Add(item:"http://google");
+        urls.Add(item:"http://youtube");
+        
+        return urls.ToArray();
+    }
+}
